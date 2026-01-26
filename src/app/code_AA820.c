d8d846a7ab823c3a2a5c1609528e20c221180fc2
Garrett Smith <smith.garrett.c@gmail.com>
2026-01-25 18:48:47 -0800

    rodata

---
+++ src/app/code_AA820.c
@@ -0,0 +1,27 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_AA820/func_803232F0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_AA820/func_80323364.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_AA820/func_803234A4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_AA820/func_80323720.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_AA820/func_80323864.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_AA820/func_8032390C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_AA820/func_803239B4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_AA820/func_80323DCC.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_AA820/func_80323FFC.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_AA820/func_803243D8.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_AA820/func_80324A34.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_AA820/func_80324AF4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_AA820/func_80324B60.s")
---
+++ src/app/code_AC1A0.c
@@ -0,0 +1,17 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_AC1A0/func_80324C70.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_AC1A0/func_80324DB4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_AC1A0/func_80324EC4.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_AC1A0/func_80325100.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_AC1A0/func_80325160.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_AC1A0/func_80325E1C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_AC1A0/func_80325F7C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/app/code_AC1A0/func_80326E40.s")
