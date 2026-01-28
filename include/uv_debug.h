d8972e1ca7653a6c494bde01a7b85e13180e842f
queueRAM <queueRAM@proton.me>
2026-01-28 14:58:47 -0800

    Add include/uv_debug.h

---
+++ include/uv_debug.h
@@ -0,0 +1,11 @@
+#ifndef UV_DEBUG_H
+#define UV_DEBUG_H
+
+#include <PR/ultratypes.h>
+
+void func_8023345C(s32, s32, s32);
+void func_80233590(void);
+void func_803182A0(void);
+void uvDbgCnt(u8, s32);
+
+#endif // UV_DEBUG_H
