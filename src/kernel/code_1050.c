aaa353e1b24a19dead352fd26088d069f9ec4f14
inspectredc <78732756+inspectredc@users.noreply.github.com>
2026-02-17 21:01:12 +0000

    Match emitter.c (#52)
    * match emitter.c
    
    * rename enum

--- src/kernel/code_1050.c
+++ src/kernel/code_1050.c
@@ -1,27 +1,16 @@
 #include <uv_audio.h>
 #include <uv_matrix.h>
+#include <libc/stdarg.h>
 
-typedef struct UnkStruct_80200144 {
-    Mtx4F mat;
-    float vec[2];
-    u8 pad48[4];
-    s32 unk4C;
-} UnkStruct_80200144_t;
-
-typedef struct UnkStruct_80200308 {
-    f32 unk[0x14];
-} UnkStruct_80200308_t;
-
-extern UnkStruct_80200308_t D_80250EC4[];
-extern UnkStruct_80200144_t D_80250E80[];
+UnkStruct_80200144_t D_80250E80[1];
 
 void func_802000DC(u16);
 
 void func_802000A0(void) {
-    s32 var_s0;
+    u16 i;
 
-    for (var_s0 = 0; var_s0 <= 0; var_s0 = (var_s0 + 1) & 0xFFFF) {
-        func_802000DC(var_s0);
+    for (i = 0; i < 1; i++) {
+        func_802000DC(i);
     }
 }
 
@@ -34,17 +23,48 @@ void func_802000DC(u16 arg0) {
     uvMat4SetIdentity(&ident);
     uvMat4Copy(&temp_a0->mat, &ident);
     temp_a0->unk4C = 2;
-    temp_a0->vec[0] = 1.0f;
-    temp_a0->vec[1] = 1.0f;
+    temp_a0->unk40 = 1.0f;
+    temp_a0->unk44 = 1.0f;
 }
 
 void func_80200144(u16 arg0, Mtx4F* arg1) {
     uvMat4Copy(&D_80250E80[arg0].mat, arg1);
 }
 
-#pragma GLOBAL_ASM("asm/nonmatchings/kernel/code_1050/func_80200180.s")
+void func_80200180(u16 arg0, ...) {
+    UnkStruct_80200144_t* temp_a2;
+    u32 propertyType;
+    va_list args;
+
+    temp_a2 = &D_80250E80[arg0];
+
+    va_start(args, arg0);
+
+    while (TRUE) {
+        propertyType = va_arg(args, s32);
+        switch (propertyType) {
+        case 0:
+            return;
+        case 1:
+            temp_a2->unk40 = va_arg(args, f64);
+            break;
+        case 2:
+            temp_a2->unk44 = va_arg(args, f64);
+            break;
+        case 4:
+            temp_a2->unk48 = va_arg(args, f64);
+            break;
+        case 8:
+            temp_a2->unk4C = va_arg(args, s32);
+            break;
+        default:
+            _uvDebugPrintf("Unknown property type %d\n", propertyType);
+            return;
+        }
+    }
+}
 
 void func_80200308(u16 arg0) {
     uvEmitterFlush(arg0);
-    uvaSeqSetVol(D_80250EC4[arg0].unk[0]);
+    uvaSeqSetVol(D_80250E80[arg0].unk44);
 }
