1fc1ab132e6c055f61aa6aec805651ff1d2235aa
Garrett Smith <smith.garrett.c@gmail.com>
2026-02-28 21:58:20 -0800

    match modified conteeplong functions

--- src/libultra/io/conteeplongread.c
+++ src/libultra/io/conteeplongread.c
@@ -1,3 +1,25 @@
-#include "common.h"
+#include "PR/os_internal.h"
+#include "PRinternal/controller.h"
 
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/io/conteeplongread/osEepromLongRead.s")
+s32 osEepromLongRead(OSMesgQueue* mq, u8 address, u8* buffer, int length) {
+    s32 ret = 0;
+
+#if BUILD_VERSION <= VERSION_D
+    if ((u8) address > 0x40) {
+        return -1;
+    }
+#endif
+
+    while (length > 0) {
+        ERRCK(osEepromRead(mq, address, buffer));
+        length -= EEPROM_BLOCK_SIZE;
+        address++;
+        buffer += EEPROM_BLOCK_SIZE;
+#if BUILD_VERSION <= VERSION_D
+        osSetTimer(&__osEepromTimer, 12000 * osClockRate / 1000000, 0, &__osEepromTimerQ, &__osEepromTimerMsg);
+        osRecvMesg(&__osEepromTimerQ, NULL, OS_MESG_BLOCK);
+#endif
+    }
+
+    return ret;
+}
--- src/libultra/io/conteeplongwrite.c
+++ src/libultra/io/conteeplongwrite.c
@@ -1,3 +1,27 @@
-#include "common.h"
+#include "PR/os_internal.h"
+#include "PRinternal/controller.h"
 
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/io/conteeplongwrite/osEepromLongWrite.s")
+s32 osEepromLongWrite(OSMesgQueue* mq, u8 address, u8* buffer, int length) {
+    s32 ret = 0;
+
+#if BUILD_VERSION <= VERSION_D
+    if ((u8) address > 0x40) {
+        return -1;
+    }
+#endif
+
+    while (length > 0) {
+        ERRCK(osEepromWrite(mq, address, buffer));
+        length -= EEPROM_BLOCK_SIZE;
+        address++;
+        buffer += EEPROM_BLOCK_SIZE;
+#if BUILD_VERSION <= VERSION_D
+        osSetTimer(&__osEepromTimer, 12000 * osClockRate / 1000000, 0, &__osEepromTimerQ, &__osEepromTimerMsg);
+#else
+        osSetTimer(&__osEepromTimer, OS_USEC_TO_CYCLES(12000), 0, &__osEepromTimerQ, &__osEepromTimerMsg);
+#endif
+        osRecvMesg(&__osEepromTimerQ, NULL, OS_MESG_BLOCK);
+    }
+
+    return ret;
+}
