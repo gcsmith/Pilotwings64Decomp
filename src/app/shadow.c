afe92e47b9332e64731f88a3abe6ce2a916b59ac
Bl00D4NGEL <github@d-peters.com>
2026-03-22 19:44:35 +0100

    match: func_803347D0

--- src/app/shadow.c
+++ src/app/shadow.c
@@ -1,7 +1,9 @@
 #include "common.h"
 #include "code_72B70.h"
+#include "kernel/code_8170.h"
 #include "shadow.h"
 #include <uv_dobj.h>
+#include <uv_math.h>
 #include <uv_model.h>
 
 // .data
@@ -174,7 +176,68 @@ void func_80334578(Mtx4F* arg0, f32 arg1) {
     uvDobjPosm(D_80350470, 0, &sp2C);
 }
 
-#pragma GLOBAL_ASM("asm/nonmatchings/app/shadow/func_803347D0.s")
+void func_803347D0(Mtx4F* arg0) {
+    Mtx4F sp58;
+    f32 sp54;
+    f32 sp50;
+    f32 sp4C;
+    f32 temp_fv0;
+    s32 pad2[2];
+    f32 temp_fv1_3;
+    f32 var_fv0;
+    f32 temp_fa1;
+    f32 temp_fa0;
+    s32* sp2C;
+    s32 pad[2];
+
+    if (D_80350470 == 0xFFFF) {
+        return;
+    }
+
+    temp_fa1 = arg0->m[1][0];
+    temp_fa0 = arg0->m[1][1];
+    sp4C = uvAtan2F(temp_fa0, temp_fa1);
+    uvMat4SetIdentity(&sp58);
+    if (D_80350464 != 0x11F && D_80350464 != 0x13C) {
+        temp_fa0 = 1.0f - SQ(arg0->m[0][2]);
+        temp_fa1 = 1.0f - SQ(arg0->m[1][2]);
+        uvMat4Scale(&sp58, temp_fa0, temp_fa1, 1 /*.0f*/);
+    }
+
+    uvMat4RotateAxis(&sp58, sp4C - 1.5707963f, 0x7A);
+    if (D_80350474 != 0.0f) {
+        temp_fv0 = uvAtan2F(1.0f, D_80350474);
+        if (arg0->m[2][2] > 0.0f) {
+            uvMat4RotateAxis(&sp58, temp_fv0, 0x78);
+        } else {
+            uvMat4RotateAxis(&sp58, temp_fv0, 0x78);
+        }
+    }
+
+    sp58.m[3][0] = arg0->m[3][0];
+    sp58.m[3][1] = arg0->m[3][1];
+    sp58.m[3][2] = arg0->m[3][2] + D_80350478;
+    sp54 = (sp58.m[3][2] * (sp58.xy * D_80350474)) + sp58.xw;
+    sp50 = (sp58.m[3][2] * (sp58.yy * D_80350474)) + sp58.yw;
+    if (uvTerraGetPt(D_80362690->terraId, sp54, sp50, &sp2C) != 0) {
+        func_80334308(D_8035046A);
+    } else {
+        func_80334308(D_80350466);
+        if (arg0->m[3][2] < 2.0f) {
+            temp_fv1_3 = arg0->m[3][2] * 0.5f;
+            if (temp_fv1_3 > 0.01f) {
+                var_fv0 = temp_fv1_3;
+            } else {
+                var_fv0 = 0.01f;
+            }
+            uvMat4Scale(&sp58, var_fv0, var_fv0, var_fv0);
+        }
+        sp58.m[3][0] = sp54;
+        sp58.m[3][1] = sp50;
+        sp58.m[3][2] = 0.0f;
+    }
+    uvDobjPosm(D_80350470, 0, &sp58);
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/app/shadow/func_80334A38.s")
 
