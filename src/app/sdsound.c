0626c25d7c204431f2fc42e16ca49112cd6d7669
Bl00D4NGEL <github@d-peters.com>
2026-03-22 23:30:52 +0100

    match: func_803339C4

--- src/app/sdsound.c
+++ src/app/sdsound.c
@@ -30,8 +30,12 @@ typedef struct {
 } Unk80371CB8; // size = 0x50 - 0x58
 extern Unk80371CB8 D_80371CB8;
 
-// foward declarations
-void func_803339C4(s32, void*, s32);
+// forward declarations
+void func_803339C4(s32, UnkSdSound*, s32);
+void func_80333D80(UnkSdSound*);
+void func_80333DDC(UnkSdSound*);
+void func_80333DF0(UnkSdSound*);
+void func_80334258(UnkSdSound*);
 
 void func_803338B0(UnkSdSound* arg0) {
     arg0->unk248 = 0.0f;
@@ -46,13 +50,83 @@ void func_803338B0(UnkSdSound* arg0) {
     D_80371CB8.unk20 = 1.0f;
     arg0->unk244 = snd_makedev(0x13);
     arg0->unk245 = snd_makedev(0x13);
-    D_80371CB0.cb = &func_803339C4;
+    D_80371CB0.cb = (EventCallback_t)&func_803339C4;
     D_80371CB0.arg = arg0;
     arg0->unk240 = -0x40;
     uvEventMaxCb(D_80371CB0, 1, 0xD, 0x12, 0x13, 0x10, 0xC, 0x16, 0x24);
 }
 
-#pragma GLOBAL_ASM("asm/nonmatchings/app/sdsound/func_803339C4.s")
+void func_803339C4(s32 arg0, UnkSdSound* arg1, s32 arg2) {
+    f32 sp3C;
+    f32 temp_ft3;
+    f32 sp34;
+    UnkSdSound* temp = arg1;
+    s32 pad;
+
+    if (arg2 != 0) {
+        _uvAssertMsg("eventData == 0", "sdsound.c", 88);
+    }
+    switch (arg0) {
+    case 13:
+        func_80334258(temp);
+        return;
+    case 12:
+        func_80333D80(temp);
+        return;
+    case 19:
+        func_80333DDC(temp);
+        return;
+    case 16:
+        temp->unk240 = -0x40;
+        return;
+    case 18:
+        func_80333D80(temp);
+        return;
+    case 22:
+        func_80333DF0(temp);
+        return;
+    case 1:
+        if (temp->unk240 & 1) {
+            return;
+        }
+
+        sp34 = 0.02f * ABS_NOEQ(temp->unk160.f[2]);
+        if (sp34 < 0.0f) {
+            sp34 = 0.0f;
+        } else if (sp34 > 1.0f) {
+            sp34 = 1.0f;
+        }
+        temp_ft3 = demoRandF() - 0.5f;
+        sp3C = (1.3f * sp34) + 0.4f + (temp_ft3 * 0.3f);
+        sp34 = (0.02f * uvVec3Len(&temp->unk160)) * (((demoRandF() * 0.4f) - 0.5f) + 1.0);
+        if (sp34 < 0.0f) {
+            sp34 = 0.0f;
+        } else if (sp34 > 1.0f) {
+            sp34 = 1.0f;
+        }
+        func_8033F904(temp->unk244, sp3C, sp34, -0.5f);
+
+        sp34 = 0.02f * ABS_NOEQ(temp->unk160.f[2]);
+        if (sp34 < 0.0f) {
+            sp34 = 0.0f;
+        } else if (sp34 > 1.0f) {
+            sp34 = 1.0f;
+        }
+        temp_ft3 = demoRandF() - 0.5f;
+        sp3C = (1.3f * sp34) + 0.4f + (temp_ft3 * 0.3f);
+        sp34 = (0.02f * uvVec3Len(&temp->unk160)) * (((demoRandF() * 0.4f) - 0.5f) + 1.0);
+        if (sp34 < 0.0f) {
+            sp34 = 0.0f;
+        } else if (sp34 > 1.0f) {
+            sp34 = 1.0f;
+        }
+        func_8033F904(temp->unk245, sp3C, sp34, 0.5f);
+        return;
+    default:
+        _uvDebugPrintf("Got unknown event type %d\n", arg0);
+        break;
+    }
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/app/sdsound/func_80333D80.s")
 
