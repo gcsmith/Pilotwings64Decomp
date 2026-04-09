3116714ad9d04e754c9320d0faf50179ae8712b1
Garrett Smith <smith.garrett.c@gmail.com>
2026-04-08 19:52:18 -0700

    fix drvrnew rodata

--- src/libultra/audio/resample.c
+++ src/libultra/audio/resample.c
@@ -3,5 +3,3 @@
 #pragma GLOBAL_ASM("asm/nonmatchings/libultra/audio/resample/alResampleParam.s")
 
 #pragma GLOBAL_ASM("asm/nonmatchings/libultra/audio/resample/alResamplePull.s")
-
-#pragma GLOBAL_ASM("asm/nonmatchings/libultra/audio/resample/D_8024FAD0.s")
