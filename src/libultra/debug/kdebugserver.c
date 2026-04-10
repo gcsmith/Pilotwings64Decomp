a35baa46f14ae1d6b4016f0f79a12dfea433759a
queueRAM <queueram@proton.me>
2026-04-10 02:50:17 +0000

    PR feedback: use pad, remove version, use header struct, static

--- src/libultra/debug/kdebugserver.c
+++ src/libultra/debug/kdebugserver.c
@@ -2,34 +2,21 @@
 #include "PR/os_internal.h"
 #include "PR/rdb.h"
 
-typedef struct {
-	unsigned type : 2;
-    unsigned unk_2 : 4;
-	unsigned length : 2;
-	char buf[3];
-} rdbPacketVD; // likely earlier version of rdbPacket
-
-static u8 buffer[0x100];
+static u8 kdebugBuffer[0x100];
+OSThread __osThreadSave;
 
 static s32 kdebugState = 0;
 static s32 numChars = 0;
 static s32 kdebugRemaining = 0;
 
-// could also be a struct with buffer at 0x20 offset
-extern u8 D_802CA750[0x1B0];
-
-#if BUILD_VERSION <= VERSION_D
-// u32 to string
-void func_802441A0(u32 arg0, u8* arg1) {
-    arg1[0] = ((arg0 >> 24) & 0xFF);
-    arg1[1] = ((arg0 >> 16) & 0xFF);
-    arg1[2] = ((arg0 >> 8) & 0xFF);
-    arg1[3] = (arg0 & 0xFF);
+UNUSED STATIC_FUNC void kdebug_u32_to_string(u32 u, u8* s) {
+    s[0] = ((u >> 24) & 0xFF);
+    s[1] = ((u >> 16) & 0xFF);
+    s[2] = ((u >> 8) & 0xFF);
+    s[3] = (u & 0xFF);
 }
-#endif
 
-// string_to_u32
-static u32 func_802441D0(u8* s) {
+STATIC_FUNC u32 kdebug_string_to_u32(u8* s) {
     u32 k;
 
     k = ((s[0] & 0xFF) << 0x18);
@@ -40,12 +27,11 @@ static u32 func_802441D0(u8* s) {
     return k;
 }
 
-#if BUILD_VERSION <= VERSION_D
 // similar to send_packet(), except for:
 //   a. different `type` bitfield width
 //   b. also waits on __osGetCause()
-void func_80244228(u8* buf, s32 length) {
-    rdbPacketVD packet;
+STATIC_FUNC void kdebug_send_packet(u8* buf, s32 length) {
+    rdbPacket packet;
     s32 i;
 
     packet.type = 0x2;
@@ -67,7 +53,7 @@ void func_80244228(u8* buf, s32 length) {
 // similar interface as send(), except:
 //   a. doesn't have __osRdb_IP6_Empty
 //   b. splits packet sending into full 3 bytes and remainder
-void func_802442E8(u8* buf, s32 length) {
+STATIC_FUNC void kdebug_send(u8* buf, s32 length) {
     s32 i;
     s32 lengthAlign3;
     s32 remainder;
@@ -81,34 +67,34 @@ void func_802442E8(u8* buf, s32 length) {
     lengthAlign3 = length - remainder;
 
     for (i = 0; i < lengthAlign3; i += 3) {
-        func_80244228(&buf[i], 3);
+        kdebug_send_packet(&buf[i], 3);
     }
     if (remainder > 0) {
-        func_80244228(&buf[lengthAlign3], remainder);
+        kdebug_send_packet(&buf[lengthAlign3], remainder);
     }
 }
 
-void kdebugserver_text_0220(void) {
+STATIC_FUNC void kdebug_send_buffer(void) {
     s32 addr;
     s32 length;
 
-    addr = func_802441D0(&buffer[1]);
-    length = func_802441D0(&buffer[5]);
-    func_802442E8((u8*)addr, length);
+    addr = kdebug_string_to_u32(&kdebugBuffer[1]);
+    length = kdebug_string_to_u32(&kdebugBuffer[5]);
+    kdebug_send((u8*)addr, length);
 }
 
-void kdebugserver_text_026C(void) {
-    func_802442E8(&D_802CA750[0x20], 0x190);
+STATIC_FUNC void kdebug_send_thread(void) {
+    kdebug_send((u8*)&__osThreadSave.context, sizeof(__osThreadSave.context));
 }
 
 void kdebugserver(u32 rawPacket) {
     u32 i;
-    rdbPacketVD packet;
+    rdbPacket packet;
 
     *(u32*)&packet = rawPacket;
 
     for (i = 0; i < packet.length; i++) {
-        buffer[numChars++] = packet.buf[i];
+        kdebugBuffer[numChars++] = packet.buf[i];
     }
     kdebugRemaining -= packet.length;
     switch (kdebugState) {
@@ -119,7 +105,7 @@ void kdebugserver(u32 rawPacket) {
             kdebugRemaining = 9 - packet.length;
             return;
         case 2:
-            kdebugserver_text_026C();
+            kdebug_send_thread();
             kdebugState = 0;
             numChars = 0;
             kdebugRemaining = 0;
@@ -133,8 +119,8 @@ void kdebugserver(u32 rawPacket) {
         break;
     case 1:
         if (kdebugRemaining <= 0) {
-            if (*buffer == 1) {
-                kdebugserver_text_0220();
+            if (kdebugBuffer[0] == 1) {
+                kdebug_send_buffer();
                 kdebugState = 0;
                 numChars = 0;
                 kdebugRemaining = 0;
@@ -153,4 +139,3 @@ void kdebugserver(u32 rawPacket) {
         break;
     }
 }
-#endif
